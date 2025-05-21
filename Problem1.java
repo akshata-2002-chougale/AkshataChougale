7949c62bb80cfcb0849a07c3268f158747a90ee4
Akshata Ramesh Chougale <chougaleakshata341@gmail.com>
2025-05-21 17:58:05 +0530

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,55 +1,48 @@
 import java.util.Scanner;
-public class Problem1 {
 
-	public static void main(String[] args) {
-		
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter value of a: ");
-		double a= sc.nextDouble();
-		System.out.print("Enter value of b: ");
-		double b=sc.nextDouble();
-		System.out.print("Enter operation (add, subtract, multiply, divide): ");
-		String operation=sc.next();
-		
-		Calculator calc = new Calculator(a, b,operation);
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of a: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter value of b: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter operation (add, subtract, multiply, divide): ");
+        String operation = sc.next();
+
+        Calculator calc = new Calculator(a, b, operation);
         calc.calculate();
 
-	}
-	
-	static class Calculator{
-		double a, b;
-		String operation;
-		
-		Calculator(double a, double b, String operation){
-			this.a=a;
-			this.b=b;
-			this.operation=operation;
-			
-		}
-		
-		void calculate() {
-			if(operation.equals("add")) {
-				System.out.println("Result: " + (a+b));
-			}
-			else if(operation.equals("sub")) {
-				System.out.println("Result:" + (a-b));
-			}
-			else if(operation.equals("mul")) {
-				System.out.println("Result: " + (a*b));
-			}
-			else if(operation.equals("divide")){
-				if(b!=0) {
-					System.out.println("Result: " + (a/b));
-				}
-				else {
-					System.out.println("error: divided by zero");
-				}
-				
-			}
-			else {
-				System.out.println("Invalid operation");
-			}
-		}
-	}
+        sc.close();
+    }
 
+    static class Calculator {
+        double a, b;
+        String operation;
+
+        Calculator(double a, double b, String operation) {
+            this.a = a;
+            this.b = b;
+            this.operation = operation;
+        }
+
+        void calculate() {
+            if (operation.equals("add")) {
+                System.out.println("Result: " + (a + b));
+            } else if (operation.equals("subtract")) {
+                System.out.println("Result: " + (a - b));
+            } else if (operation.equals("multiply")) {
+                System.out.println("Result: " + (a * b));
+            } else if (operation.equals("divide")) {
+                if (b != 0) {
+                    System.out.println("Result: " + (a / b));
+                } else {
+                    System.out.println("Error: divided by zero");
+                }
+            } else {
+                System.out.println("Invalid operation");
+            }
+        }
+    }
 }
